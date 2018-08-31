8416b7f9bc15170e50ad2a2d211da9266a39c074
tark S <344754617@qq.com>
2018-08-31 14:48:27 +0800

    1st

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const server =express();
+server.get('/',(req,res)=>{
+    res.sendFile(
+        path.resolve('index.html')
+    )
+});
+server.listen(800);
